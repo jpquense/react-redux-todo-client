4ee90c8111be87704ccf3bcf33672bc9de26472f
John P Quense <jpquense@gmail.com>
2018-04-29 16:18:31 -0400

    combine reducer function from scratch

--- src/redux/reducers/index.reducer.js
+++ src/redux/reducers/index.reducer.js
@@ -1,58 +1,38 @@
-const todoReducer = (state, action) => {
-    switch(action.type) {
-        case 'ADD_TODO':
-            return {
-                id: action.id,
-                text: action.text,
-                completed: false
-            }
-        case 'TOGGLE_TODO':
-            if (state.id !== action.id) {
-                return state;
-            }
-            return {
-                ...state,
-                completed: !state.completed
-            };
-        default:
-            return state;
-    }
-};
+import { combineReducers } from 'redux';
+import todosReducer from './todos.reducer';
+import visibilityFilterReducer from './visibilityFilter.reducer';
 
-const todosReducer = (state = [], action) => {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return [
-                ...state,
-                todoReducer(undefined, action)
-            ];
-        case 'TOGGLE_TODO':
-            return state.map(t => todoReducer(t, action));
-        default:
-            return state;
-    }
-};
+// const combineReducers = (reducers) => {
+//     return (state = {}, action) => {
+//         return Object.keys(reducers).reduce(
+//             (nextState, key) => {
+//                 nextState[key] = reducers[key](
+//                     state[key],
+//                     action
+//                 );
+//                 return nextState;
+//             },
+//             {}
+//         );
+//     };
+// };
 
-const visibilityFilterReducer = (state = 'SHOW_ALL', action) => {
-    switch (action.type) {
-        case 'SET_VISIBILITY_FILTER':
-            return action.filter;
-        default:
-            return state;
-    }
-}
+const mainReducer = combineReducers({
+    todosReducer,
+    visibilityFilterReducer
+})
 
-const todoAppReducer = (state = {}, action) => {
-    return {
-        todos: todosReducer(
-            state.todos,
-            action
-        ),
-        visibilityFilter: visibilityFilterReducer(
-            state.visibilityFilter,
-            action
-        )
-    };
-};
+// const todoAppReducer = (state = {}, action) => {
+//     return {
+//         todos: todosReducer(
+//             state.todos,
+//             action
+//         ),
+//         visibilityFilter: visibilityFilterReducer(
+//             state.visibilityFilter,
+//             action
+//         )
+//     };
+// };
 
-export default todoAppReducer;
+export default mainReducer;
---
+++ src/redux/reducers/todos.reducer.jsx
@@ -0,0 +1,36 @@
+const todoReducer = (state, action) => {
+    switch(action.type) {
+        case 'ADD_TODO':
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false
+            }
+        case 'TOGGLE_TODO':
+            if (state.id !== action.id) {
+                return state;
+            }
+            return {
+                ...state,
+                completed: !state.completed
+            };
+        default:
+            return state;
+    }
+};
+
+const todosReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [
+                ...state,
+                todoReducer(undefined, action)
+            ];
+        case 'TOGGLE_TODO':
+            return state.map(t => todoReducer(t, action));
+        default:
+            return state;
+    }
+};
+
+export default todosReducer;
