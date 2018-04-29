38fea606f615b6e101d9d9f07ac744088d31c0e0
John P Quense <jpquense@gmail.com>
2018-04-29 09:56:01 -0400

    reducer for todo and todos added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const deepFreeze = require('deep-freeze');
+
+const todo = (state, action) => {
+    switch(action.type) {
+        case 'ADD_TODO':
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false
+            }
+        case 'TOGGLE_TODO':
+            if (state.id !== action.it) {
+                return state;
+            }
+            return {
+                ...state,
+                completed: !state.completed
+            };
+        default:
+            return state;
+    }
+};
+
+const todosReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [
+                ...state,
+                todo(undefined, action)
+            ];
+        case 'TOGGLE_TODO':
+            return state.map(t => todo(t, action));
+        default:
+            return state;
+    }
+};
